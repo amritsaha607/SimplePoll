9200103e0cb4ee9ef9038f18730426fb5262e8c9
Amrit Saha <amritsaha607@gmail.com>
2022-02-02 21:22:41 +0530

    added models

--- PollApp/models.py
+++ PollApp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Choice(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Poll(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    choices = models.ManyToManyField(
+        Choice, related_name='related_polls', blank=True)
+
+    def __str__(self):
+        return self.name
